f1772cc6535826bc4af3a01e23ca1298f91073c0
C <cmrice@ucsd.edu>
2025-07-13 13:51:27 -0700

    Staged changes for mongodb passcode (testing) and some dependency revision

--- Makefile
+++ Makefile
@@ -1,11 +1,10 @@
-ENTRYPOINT = ...
-
-$(ENTRYPOINT).java : 
-	java -jar build/libs/Job_Spreadsheet.jar
-
 #  Can't get the run command to work with wsl. To run, execute "./gradlew run" in a powershell/cmd terminal
-run :
-	java -jar ./build/libs/Job_Spreadsheet.jar
+# ENTRYPOINT = ...
+# $(ENTRYPOINT).java : 
+# 	java -jar build/libs/Job_Spreadsheet.jar
+# run :
+# 	java -jar ./build/libs/Job_Spreadsheet.jar
+
 gbuild :
 	./gradlew build
 gclear :
--- src/main/java/Test/App.java
+++ src/main/java/Test/App.java
@@ -1,29 +1,34 @@
 // package main.java.Test;
+
+// Use following if running on powershell
 package Test;
 
 import javafx.application.Application;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-
-// class EntryPoint {
-//     public static void main(String[] args) {
-//         System.out.println("Setup Works!");
-//     }
-// }
 
 public class App extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-        Label label = new Label("Hello, JavaFX!");
+        Button button = new Button("Click Me");
+        button.getStyleClass().add("test-button");
 
-        StackPane root = new StackPane(label);
+        VBox root = new VBox(button);
+        root.setAlignment(Pos.CENTER);
+        root.setSpacing(10);
+
         Scene scene = new Scene(root, 300, 200);
+        scene.getStylesheets().add(getClass().getResource("/style.css").toExternalForm());
+        
 
-        primaryStage.setTitle("JavaFX Test");
         primaryStage.setScene(scene);
+        primaryStage.setTitle("JavaFX CSS Example");
         primaryStage.show();
     }
 
